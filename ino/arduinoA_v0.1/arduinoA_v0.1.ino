d5961af5b2a1baa3ffedd93f1e38e9c0054db387
Niklas Buhl <nibuh@dtu.dk>
2017-03-24 21:41:37 +0100

    coolstuff

--- ino/arduinoA_v0.1/arduinoA_v0.1.ino
+++ ino/arduinoA_v0.1/arduinoA_v0.1.ino
@@ -2,6 +2,8 @@
 
 void setup() {
 
+  pinMode(13, OUTPUT);
+
   if(DEBUG_MODE && CONNECTED_TO_PC) {
       Serial.begin(115200);
       while(!Serial);
@@ -9,9 +11,23 @@ void setup() {
 
   }
 
+  // Initialize Instuction Buffer
+
+  // Communicate to RPi
+
+  // Ready!
+
 }
 
 void loop() {
+
+  digitalWrite(13, HIGH);
+
+  delay(200);
+
+  digitalWrite(13, LOW);
+
+  delay(200);
 
   // Listen to I2C
 
